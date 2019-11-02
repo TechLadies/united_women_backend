bd88badc29d81eb8c4cc362968fe10e2eeb897b5
Hui Xuan Tan <s10188206@connect.np.edu.sg>
2019-11-02 17:29:25 +0800

    Use env variables for database

---
+++ db/config.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    database: "united_women_dev",
+    host: "localhost",
+    dialect: "postgres",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD
+  },
+  test: {
+    database: "united_women_test",
+    host: "localhost",
+    dialect: "postgres",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD
+  },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    ssl: true,
+    dialectOptions: {
+      ssl: true
+    }
+  }
+};
