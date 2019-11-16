0d971aaf1efe4777e511fac14511685e692d3eee
Weiyuan Liu <weiyuan.lane@gmail.com>
2019-11-16 23:45:17 +0800

    [Ops] Allow for config to retrieve host and db name from .env

--- db/config.js
+++ db/config.js
@@ -1,23 +1,23 @@
-require("dotenv").config()
+require('dotenv').config()
 
 module.exports = {
   development: {
-    database: "united_women_dev",
-    host: "localhost",
-    dialect: "postgres",
+    database: process.env.DB_NAME || 'united_women_dev',
+    host: process.env.DB_SERVER || 'localhost',
+    dialect: 'postgres',
     username: process.env.DB_USERNAME || 'postgres',
     password: process.env.DB_PASSWORD
   },
   test: {
-    database: "united_women_test",
-    host: "localhost",
-    dialect: "postgres",
+    database: 'united_women_test',
+    host: 'localhost',
+    dialect: 'postgres',
     username: process.env.DB_USERNAME || 'postgres',
     password: process.env.DB_PASSWORD
   },
   production: {
-    use_env_variable: "DATABASE_URL",
-    dialect: "postgres",
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
     ssl: true,
     dialectOptions: {
       ssl: true
