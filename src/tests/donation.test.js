095caf92d0b78012a4db75e42bbc63eb05049cbe
Hui Xuan Tan <s10188206@connect.np.edu.sg>
2019-12-04 11:28:37 +0800

    modified test code to include check for authorization

--- src/tests/donation.test.js
+++ src/tests/donation.test.js
@@ -1,17 +1,35 @@
+const path = require("path");
 const supertest = require("supertest");
 const app = require("../app");
 const request = supertest(app);
-const pagination = require('../middlewares/pagination')
+const auth = require(path.resolve("src/auth/auth"));
+const pagination = require("../middlewares/pagination");
+const login = require("../tests/login.test");
 
 describe("Donation Get Endpoint", () => {
+  // When valid token is supplied
   it("should display list of donations successfully", async done => {
-    const res = await request.get("/donations", pagination);
+    let res = await login.post("/login", login.body, auth.login);
+    const token = res.body.token;
+    res = await request
+      .get("/donations", auth.required, pagination)
+      .set("Authorization", "Bearer " + token);
     expect(res.statusCode).toBe(200);
-    expect(res.body).toEqual(expect.objectContaining({
-        "data": expect.any(Array),
-        "perPage": expect.any(Number),
-        "offset": expect.any(Number)
-    }));
+    expect(res.body).toEqual(
+      expect.objectContaining({
+        data: expect.any(Array),
+        perPage: expect.any(Number),
+        offset: expect.any(Number)
+      })
+    );
+    done();
+  });
+
+  // When valid token is not supplied
+  it("should return an error message", async done => {
+    const res = await request.get("/donations", auth.required, pagination);
+    expect(res.statusCode).toBe(403);
+    expect(res.body.message).toBe("403 forbidden");
     done();
   });
 });
--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -4,14 +4,27 @@ const login = require("../auth/auth").login;
 const passport = require("passport");
 const request = supertest(app);
 
+function post(url, body, middleware) {
+  const httpRequest = request.post(url);
+  httpRequest.send(body);
+  return httpRequest;
+}
+
+let body = {
+  username: "tan@uws.org.sg",
+  password: "password123"
+};
+
 describe("Login Post Endpoint", () => {
   it("should login successfully", async done => {
-    const res = await request.post("/login", login).send({
-      username: "tan@uws.org.sg",
-      password: "password123"
-    });
+    const res = await post("/login", body, login);
     expect(res.statusCode).toBe(200);
     expect(res.body.token).toEqual(expect.any(String));
     done();
   });
 });
+
+module.exports = {
+  post,
+  body
+}
